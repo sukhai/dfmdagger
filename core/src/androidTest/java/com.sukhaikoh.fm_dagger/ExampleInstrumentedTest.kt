f7bbef03f18e61c311b57c9d75676e6da535b7db
Su Khai Koh <8684653+sukhai@users.noreply.github.com>
2019-09-27 20:29:56 -0700

    Initial creation of core module

--- core/src/androidTest/java/com.sukhaikoh.fm_dagger/ExampleInstrumentedTest.kt
+++ core/src/androidTest/java/com.sukhaikoh.fm_dagger/ExampleInstrumentedTest.kt
@@ -2,8 +2,8 @@ package com.sukhaikoh.fm_dagger
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import org.junit.runner.RunWith
 
 /**
